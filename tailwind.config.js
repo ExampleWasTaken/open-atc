96b6b337f86f4df6f03470dca0cc402f1c1c18e8
ExampleWasTaken <58574351+ExampleWasTaken@users.noreply.github.com>
2023-12-29 15:20:49 +0100

    add initial color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,18 @@
 export default {
   content: ['./index.html', './src/**/*.{ts,tsx,js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: '#010909',
+        white: '#ffffff',
+        primary: '#d8ca28',
+        secondary: '#1c41e3',
+        accent: '#6453ea',
+        positive: '#00c853',
+        warning: '#ffc107',
+        negative: '#ff3d00',
+      },
+    },
   },
   plugins: [],
 }
-
